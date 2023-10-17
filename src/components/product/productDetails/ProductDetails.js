0f4408201367c827a78aa2e0ef70581bf915e1e6
takimi12 <tomek12olech@gmail.com>
2023-10-18 00:01:39 +0200

    Wyświetlanie pojedynczego produktu na product details

--- src/components/product/productDetails/ProductDetails.js
+++ src/components/product/productDetails/ProductDetails.js
@@ -1,8 +1,10 @@
 import { doc, getDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { db } from '../../../firebase/config'
+import styles from './ProductDetails.module.scss'
+import spinnerImg from "../../../assets/img/spinner.jpg"
 
 const ProductDetails = () => {
   const { id } = useParams()
@@ -27,14 +29,49 @@ const ProductDetails = () => {
      setProduct(obj)
 
     } else {
-      toast.erorr("Product not found")
+      toast.error("Product not found")
     }
 
   };
   return (
     <div>
-      <h1>Product Details</h1>
-      <p>{id}</p>
+      <div className={`container${styles.product}`}>
+        <h2>Product Details</h2>
+        <Link to="$/products">&larr; Back products</Link>
+      </div>
+      {product === null ? (
+        <img src={spinnerImg} alt="Loading" style={{width:"50px"}}/>
+      ) : (
+        <>
+        <div className={styles.details}>
+          <div className={styles.img}>
+            <img src={product.imageURL} alt={product.name} />
+            </div>
+            <div className={styles.content}>
+              <h3>{product.name}</h3>
+              <p className={styles.price}>
+                {`$${product.price}`}
+              </p>
+              <p>{product.desc}</p>
+              <p>
+                <b>SKU</b> {product.id}
+              </p>
+              <p>
+                <b>Brand</b> {product.brand}
+              </p>
+              <div className={styles.count}>
+                <button className='--btn '>-</button>
+                <p>
+                  <b>1</b>
+                </p>
+                <button className='--btn '>+</button>
+              </div>
+              <button className='--btn --btn-primary'>Add to cart</button>
+            </div>
+        </div>
+        </>
+      )
+    }
     </div>
   )
 }
