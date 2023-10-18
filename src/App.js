84c3855c95d6a3623f97ab2684cf55b9b8bfa8ef
takimi12 <tomek12olech@gmail.com>
2023-10-19 00:25:33 +0200

    Checkout Komponent

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import UserLogin from './components/userOnlyRoute/userOnlyRoute';
 import Home from './pages/home/Home';
 import ProductDetails from './components/product/productDetails/ProductDetails';
 import Cart from './pages/cart/Cart';
+import CheckoutDetails from './pages/checkout/CheckoutDetails';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           } />
           <Route path="/product-details/:id" element={<ProductDetails />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout-details" element={<CheckoutDetails />} />
           {/* Dodaj Route dla Home na stronie głównej */}
           <Route path="/" element={<Home />} />
         </Routes>
