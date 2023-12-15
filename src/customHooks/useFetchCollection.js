9d423e942655ee9776671b44d07bfc9b0bcc1529
takimi12 <tomek12olech@gmail.com>
2023-12-16 00:43:03 +0100

    use fetch collection v.2

--- src/customHooks/useFetchCollection.js
+++ src/customHooks/useFetchCollection.js
@@ -6,40 +6,37 @@ import { db } from "../firebase/config";
 const useFetchCollection = (collectionName) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  
 
-  const getCollection = () => {
-    setIsLoading(true);
-    try {
-      const docRef = collection(db, collectionName);
-      const q = query(docRef, orderBy("createdAt", "desc"));
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const docRef = collection(db, collectionName);
+        const q = query(docRef, orderBy("createdAt", "desc"));
 
-      onSnapshot(q, (snapshot) => {
-        try {
+        const unsubscribe = onSnapshot(q, (snapshot) => {
           const allData = snapshot.docs.map((doc) => ({
             id: doc.id,
             ...doc.data(),
           }));
           setData(allData);
           setIsLoading(false);
-        } catch (error) {
-          setIsLoading(false);
-          toast.error("Error fetching data: " + error.message);
-        }
-      });
-      
-    } catch (error) {
-      setIsLoading(false);
-      toast.error(error.message);
-    }
-  };
+        });
 
-  useEffect(() => {
-    getCollection();
-  }, []);
+        return () => {
+          // Czyszczenie subskrypcji w przypadku odmontowania komponentu
+          unsubscribe();
+        };
+      } catch (error) {
+        setIsLoading(false);
+        toast.error("Error fetching data: " + error.message);
+      }
+    };
+
+    fetchData();
+  }, [collectionName]); // Dodaj zależność od collectionName
 
   return { data, isLoading };
 };
 
 export default useFetchCollection;
-
