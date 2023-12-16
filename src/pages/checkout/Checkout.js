9e7744bddc99d97702fbdbe2810bb1b06b76e3df
takimi12 <tomek12olech@gmail.com>
2023-12-16 21:36:12 +0100

    Create payment intent v2.

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -8,7 +8,10 @@ import { selectBillingAddress, selectShippingAddress } from "../../redux/slice/c
 import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
 import { toast } from "react-toastify";
 
+
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+
+
 
 const Checkout = () => {
     const [message, setMessage] = useState("Initializing checkout...");
@@ -22,63 +25,64 @@ const Checkout = () => {
 
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(CALCULATE_SUBTOTAL());
-        dispatch(CALCULATE_TOTAL_QUANTITY());
+    useEffect(() =>{
+        dispatch(CALCULATE_SUBTOTAL())
+        dispatch(CALCULATE_TOTAL_QUANTITY())
     }, [dispatch, cartItems]);
 
-    const description = `eShop payment: email:${customerEmail}, Amount: ${totalAmount}`;
+    const description = `eShop payment: email:${customerEmail}, Amount: ${totalAmount}`
+
 
     useEffect(() => {
-        // Create PaymentIntent as soon as the page loads
-        fetch("/api/create-payment-intent", {  // Zmiana adresu URL na Vercelowy endpoint
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                items: cartItems,
-                userEmail: customerEmail,
-                shipping: shippingAddress,
-                billing: billingAddress,
-                description
-            }),
+      // Create PaymentIntent as soon as the page loads
+      fetch("https://react-ecomerce-vjbc.vercel.app//create-payment-intent", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+             items: cartItems,
+             userEmail:customerEmail,
+             shipping:shippingAddress,
+             billing:billingAddress,
+             description
+             }),
+      })
+        .then((res) => {
+            if (res.ok) {
+                return res.json()
+            }
+            return res.json().then((json) => Promise.reject(json))
         })
-            .then((res) => {
-                if (res.ok) {
-                    return res.json();
-                }
-                return res.json().then((json) => Promise.reject(json));
-            })
-            .then((data) => {
-                setClientSecret(data.clientSecret);
-            })
-            .catch((error) => {
-                setMessage("Failed to initialize checkout");
-                toast.error("Something went wrong");
-            });
+        .then((data) => {
+            setClientSecret(data.clientSecret)
+        })
+        .catch((error) => {
+            setMessage("Failed to initialize checkout")
+            toast.error("Something went wrong")
+        })
     }, []);
-
+  
     const appearance = {
-        theme: 'stripe',
+      theme: 'stripe',
     };
     const options = {
-        clientSecret,
-        appearance,
+      clientSecret,
+      appearance,
     };
-
-    return (
-        <>
-            <section>
-                <div className="container">
-                    {!clientSecret && <h3>{message}</h3>}
-                </div>
-            </section>
-            {clientSecret && (
-                <Elements options={options} stripe={stripePromise}>
-                    <CheckoutForm />
-                </Elements>
-            )}
-        </>
-    );
-};
+  return (
+  <>
+    <section>
+        <div className="container">
+            {!clientSecret && <h3>{message}</h3>}
+        </div>
+        
+    </section>
+    {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+  </>
+  )
+}
 
 export default Checkout;
